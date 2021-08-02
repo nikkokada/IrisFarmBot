e78f3e19e74a7689478f986535df30cef9f68ade
nikkokada <88064000+nikkokada@users.noreply.github.com>
2021-08-02 23:37:59 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,17 +1,17 @@
 from os import environ
 
 # Настройки записи, можно не менять, но на всякий случай
-OWNER_ID = -174105461
-POST_ID = 6713149
+OWNER_ID = -551849022
+POST_ID = 340
 
 # Токен(ы) пользователей
 TOKENS = environ.get('TOKENS').split(',')
 
 # Задержка в секундах (на данный момент 4 часа и 15 секунд)
-DELAY = (60 * 60 * 4) + 15
+DELAY = (1 * 1 * 1) + 5
 
 # Удалять коментарий? (True - да, False - нет)
-DELETE_COMMENTS = True
+DELETE_COMMENTS = False
 
 # Задержка перед удалением коментариев (в секундах, не стоит ставить меньше 5)
 DELAY_BEFORE_DELETION = 10
